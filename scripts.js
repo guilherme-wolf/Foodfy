54f027f5a9fdf27907ffebec700b5f320e30a2a0
guilherme-wolf <63266766+guilherme-wolf@users.noreply.github.com>
2020-06-23 20:26:07 -0300

    new work today

--- scripts.js
+++ scripts.js
@@ -4,10 +4,17 @@ const cards = document.querySelectorAll ('.card');
 
 for (let card of cards) {
     card.addEventListener("click" , function (){
+        const imageId = card.getAttribute("id")
         modalOverlay.classList.add('active')
+        modalOverlay.querySelector('img').src =`/image/assets/${imageId}.png`
+        const TextTitle = document.querySelector(".title")
+        modalOverlay.querySelector("h3").textContent = TextTitle
+
     } )
 }
 
 document.querySelector('.close-modal').addEventListener("click", function(){
     modalOverlay.classList.remove ('active')
 })
+
+
