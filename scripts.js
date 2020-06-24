2237e07579539aa6aa4b6f81008d2c4da4c59251
guilherme-wolf <63266766+guilherme-wolf@users.noreply.github.com>
2020-06-24 20:35:31 -0300

    finalizei o desafio foodfy

--- scripts.js
+++ scripts.js
@@ -1,20 +1,21 @@
 const modalOverlay = document.querySelector ('.modal-overlay');
 const cards = document.querySelectorAll ('.card');
+const recipes = document.querySelectorAll (".recipe");
 
 
 for (let card of cards) {
     card.addEventListener("click" , function (){
+        const title = card.querySelector("h3").textContent
+        const autor = card.querySelector("p").textContent
         const imageId = card.getAttribute("id")
         modalOverlay.classList.add('active')
         modalOverlay.querySelector('img').src =`/image/assets/${imageId}.png`
-        const TextTitle = document.querySelector(".title")
-        modalOverlay.querySelector("h3").textContent = TextTitle
-
+        modalOverlay.querySelector('.modal-title h3').innerHTML = title
+        modalOverlay.querySelector('.autor p').innerHTML = autor
     } )
 }
 
 document.querySelector('.close-modal').addEventListener("click", function(){
     modalOverlay.classList.remove ('active')
 })
-
 
